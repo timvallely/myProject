a09268d878ab9f755317b2d4b7291174cc2dec44
timvallely <timvallely@hotmail.com>
2016-11-16 12:55:40 -0500

    More updates

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
-var http = require("http");
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
+// listener #1
+var listner1 = function listner1() {
+   console.log('listner1 executed.');
+}
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+// listener #2
+var listner2 = function listner2() {
+  console.log('listner2 executed.');
+}
+
+// Bind the connection event with the listner1 function
+eventEmitter.addListener('connection', listner1);
+
+// Bind the connection event with the listner2 function
+eventEmitter.on('connection', listner2);
+
+var eventListeners = eventEmitter.listenerCount(eventEmitter,'connection');
+
+console.log(eventListeners + " Listner(s) listening to connection event");
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+// Remove the binding of listner1 function
+eventEmitter.removeListener('connection', listner1);
+console.log("Listner1 will not listen now.");
+
+// Fire the connection event 
+eventEmitter.emit('connection');
+
+eventListeners = eventEmitter.listenerCount(eventEmitter,'connection');
+console.log(eventListeners + " Listner(s) listening to connection event");
+
+console.log("Program Ended.");
